ac901e334e1107cdc68b0fef87b4b1a44d13882b
S1umish <sunitee.mishra@gmail.com>
2020-12-16 14:16:15 -0500

    updated mission controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
-User.destroy_all
-Mission.destroy_all
 
+User.create(username: "test2", password: "pw")
+User.create(username: "test3", password: "pw")
+User.create(username: "test4", password: "pw")
 
 users.each do |user|
     User.create(user)
 end
-
-
-test1 = User.create(username: "test2", password: "pw")
-test2 = User.create(username: "test3", password: "pw")
-test3 = User.create(username: "test4", password: "pw")
-
 
 Mission.create(title: "Red Cross", description: "Blood donation camp setup", user_id: test1.id)
 Mission.create(title: "Gleaners", description: "Food bank -sort and pack food", user_id: test1.id)
