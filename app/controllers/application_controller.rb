361e4704def3fb1124ae7be046c107b9f56c6bd9
S1umish <sunitee.mishra@gmail.com>
2020-12-17 18:40:57 -0500

    updated app controller with authorized to edit method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,10 @@ class ApplicationController < Sinatra::Base
         def current_user
            @current_user ||= User.find_by(id: session[:user_id])
         end
+
+        def authorized_to_edit?(mission)
+            mission.user == current_user
+        end
         
     end 
 
